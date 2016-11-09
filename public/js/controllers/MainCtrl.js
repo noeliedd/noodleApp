b1147e58cc561f2598dc8ee0b3209790613b4e30
noeliedd <you@example.comnoel.devitt@student.ncirl.ie>
2016-11-09 12:44:19 +0000

    commit all files

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,5 +1,39 @@
-angular.module('MainCtrl', []).controller('MainController', function($scope) {
-
-	$scope.tagline = 'To the moon and back!';	
-
+angular.module('MainCtrl', []).controller('CarouselDemoCtrl', function($scope) {
+  $scope.myInterval = 5000;
+  $scope.desc = ['Nam Sai Noodles','Dark Soup','Tom Seb','Aroi Jing Jing','E-haa','Gan Tad','Khan Hee','Moi Pong','I Sad','Ham Yai','Ham Lek'];
+  $scope.slides = [
+    {
+      image: 'img/menuA.png'
+    },
+    {
+      image: 'img/menuB.png'
+    },
+    {
+      image: 'img/menuC.png'
+    },
+    {
+      image: 'img/menuD.png'
+    },
+    {
+      image: 'img/menuE.png'
+    },
+    {
+      image: 'img/menuF.png'
+    },
+    {
+      image: 'img/menuG.png'
+    },
+    {
+      image: 'img/menuH.png'
+    },
+    {
+      image: 'img/menuI.png'
+    },
+    {
+      image: 'img/menuJ.png'
+    },
+    {
+      image: 'img/menuK.png'
+    }    
+  ];
 });
--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 // modules =================================================
 var express        = require('express');
 var app            = express();
-var mongoose       = require('mongoose');
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
 
 // configuration ===========================================
 	
 // config files
-var db = require('./config/db');
+// var db = require('./config/db');
 
-var port = process.env.PORT || 8080; // set our port
+var port = process.env.PORT || 3000; // set our port
 // mongoose.connect(db.url); // connect to our mongoDB database (commented out after you enter in your own credentials)
 
 // get all data/stuff of the body (POST) parameters
